aba424494f2d32b9e30695a1f877627658ffe36a
lylejover <lylejover2yahoo.com>
2016-02-23 17:55:45 +0800

    Added Pokemen Cell View class

---
+++ Pokedex/PokeCellCollectionViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PokeCellCollectionViewCell.swift
+//  Pokedex
+//
+//  Created by Lyle Christianne Jover on 23/02/2016.
+//  Copyright © 2016 Lyle Christianne Jover. All rights reserved.
+//
+
+import UIKit
+
+class PokeCellCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    func configureCell(pokemon: Pokemon) {
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalizedString
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+        
+    }
+}
